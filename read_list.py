92cb3cf068df893a826d70bf1250458ed98cf651
TereBin <44892691+TereBin@users.noreply.github.com>
2023-05-05 02:56:02 +0900

    Update read_list.py

--- read_list.py
+++ read_list.py
@@ -1,6 +1,6 @@
 import json
 
-async def read_list(streamer_json_path):
-    with open(streamer_json_path, 'r', encoding='utf-8') as f:
-        streamer_dict = json.load(f)  # make streamer_json to streamer_dict
-    return streamer_dict
+async def read_list(json_path):
+    with open(json_path, 'r', encoding='utf-8') as f:
+        dictionary = json.load(f)  # make streamer_json to streamer_dict
+    return dictionary
